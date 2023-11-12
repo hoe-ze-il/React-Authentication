9f7bacde60f6bc13d990acf940494ff3f3035b8c
Hoe Ze Il <hoezeil20@gmail.com>
2023-11-12 23:30:38 +0700

    First Commit

---
+++ src/components/PersistLogin.js
@@ -0,0 +1,35 @@
+import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
+import useRefreshToken from "../hooks/useRefreshToken";
+import useAuth from "../hooks/useAuth";
+
+const PersistLogin = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const refresh = useRefreshToken();
+    const { auth, persist } = useAuth();
+
+    useEffect(() => {
+        const verifyRefreshToken = async () => {
+            try {
+                await refresh();
+            } catch (err) {
+                console.error(err);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        !auth?.accessToken ? verifyRefreshToken() : setIsLoading(false);
+    }, []);
+
+    useEffect(() => {
+        console.log(`isLoading: ${isLoading}`);
+        console.log(`aT: ${JSON.stringify(auth?.accessToken)}`);
+    }, [isLoading]);
+
+    return (
+        <>{!persist ? <Outlet /> : isLoading ? <p>Loading..</p> : <Outlet />}</>
+    );
+};
+
+export default PersistLogin;
